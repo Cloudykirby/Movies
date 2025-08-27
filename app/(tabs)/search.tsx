5393d4a9a73d6b74698893799234dd899570a3a5
CloudyKirby <kirbyc6@gmail.com>
2025-08-26 20:09:26 -0400

    added api

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Search = () => {
+  return (
+    <View className='flex-1 justify-center items-center '>
+      <Text>Search</Text>
+    </View>
+  );
+};
+
+export default Search;
---
+++ app/movies/[ids].tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const MovieDetails = () => {
+    return (
+        <View className="flex-1 justify-center items-center "
+        >
+            <Text>MovieDetails</Text>
+        </View>
+    );
+}
+
+
+export default MovieDetails;
